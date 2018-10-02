28a2c930c5e8f79b1a0492f3602c459889ecbc17
Marco Salathe <marco.salathe@gmail.com>
2018-10-02 10:35:49 -0700

    Small improvement to also make develop and python work

--- install.sh
+++ install.sh
@@ -4,11 +4,15 @@ INSTALL_PROTOC=$PWD/scripts/install_protoc.sh
 MODELS_DIR=$PWD/third_party/models
 
 PYTHON=python
-
-if [ $# -eq 1 ]; then
-  PYTHON=$1
-fi
-
+INSTALL="install"
+for arg do
+  shift
+  case $arg in
+    develop) INSTALL="develop";;
+    *python*) PYTHON=$arg;;
+    *) set -- "$@" "$arg";;
+  esac
+done
 echo $PYTHON
 
 # install protoc
@@ -24,15 +28,15 @@ echo $PWD
 echo "Installing object detection library"
 echo $PROTOC
 $PROTOC object_detection/protos/*.proto --python_out=.
-$PYTHON setup.py install $@
+echo $PYTHON setup.py $INSTALL $@
 popd
 
 pushd $MODELS_DIR/research/slim
 echo $PWD
 echo "Installing slim library"
-$PYTHON setup.py install $@
+echo $PYTHON setup.py $INSTALL $@
 popd
 
 echo "Installing tf_trt_models"
 echo $PWD
-$PYTHON setup.py install $@
+echo $PYTHON setup.py $INSTALL $@
